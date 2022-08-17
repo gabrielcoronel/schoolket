55695a5945cfe5764e7f8e40ec2e8ade08c67171
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2022-08-16 20:49:53 -0600

    Añadí cambios anteriores

--- client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Field from './Field.jsx';
-import FileChooser from './FileChooser.jsx';
-import ErrorBox from "./ErrorBox.jsx";
-import TextArea from './TextArea.jsx';
-
-export { Field, FileChooser, ErrorBox, TextArea };
--- client/src/index.js
+++ client/src/index.js
@@ -1,55 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LogIn from './LogIn.jsx';
-import CreateProduct from './CreateProduct.jsx';
-import './tailwind.css';
-
-import { useState, useEffect } from 'react';
-
-const useCounter = (start) => {
-  const [count, setCount] = useState(start);
-  const interval = setInterval(() => setCount(count + 1), 1000);
-
-  useEffect(() => {
-    return () => clearInterval(interval);
-  });
-
-  return count;
-};
-
-const Counter = ({ start, className }) => {
-  const count = useCounter(Number(start));
-
-  return (
-    <div className={className}>
-      {count}
-    </div>
-  );
-};
-
-const MyCounter = () => {
-  return (
-    <div className="flex justify-center">
-      <Counter
-        start="0"
-        className="bg-[#0000ff] text-white w-1/6 h-60 flex justify-center items-center"
-      />
-    </div>
-  );
-};
-
-const MyPage = () => {
-  return (
-    <>
-      This is my Counter
-
-      <MyCounter />
-    </>
-  )
-};
+import LogIn from './LogIn/LogIn.jsx';
+import CreateProduct from './CreateProduct/CreateProduct.jsx';
+import Profile from './Profile/Profile.jsx';
 
 const root = ReactDOM.createRoot(
   document.querySelector("#root")
 );
 
-root.render(<MyPage />);
+root.render(
+  <div>
+    <Profile username={"lupeeee"}/>
+  </div>
+);
