cb60f24129be5c6fe29076803ca4b5776840542b
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2022-09-26 17:57:29 -0600

    Cambié el tamaño del botón para hacer una nueva publicación

--- client/src/MyProducts/AddProduct.jsx
+++ client/src/MyProducts/AddProduct.jsx
@@ -24,7 +24,7 @@ const Text = () => {
 
 const AddProduct = () => {
   return (
-    <div className="bg-white grid grid-cols-3 gap-2 p-4 w-1/2 border border-slate-300 rounded-xl shadow-2xl">
+    <div className="bg-white grid grid-cols-3 gap-2 p-4 w-1/3 border border-slate-300 rounded-xl shadow-2xl">
       <Button />
 
       <Text />
