3ecb3d5f8bc1bcbe608c84a50804a2da27a915c6
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2022-09-26 18:33:09 -0600

    Le añadí estilo a los botones de desmarcar/marcar como vendido

--- client/src/Product/SoldButton.jsx
+++ client/src/Product/SoldButton.jsx
@@ -2,6 +2,22 @@ import { toggleIsSold } from "../util/server-util.js";
 import { IconedLabel } from "../general-components";
 import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 
+const soldClasses = `
+  bg-red-600
+  text-white
+
+  hover:bg-red-400
+  hover:text-black
+`;
+
+const unsoldClasses = `
+  bg-green-700
+  text-white
+
+  hover:bg-green-400
+  hover:text-black
+`;
+
 const SoldButton = ({ product_id, isSold, updateIsSold }) => {
   const handleClick = () => {
     toggleIsSold(product_id);
@@ -11,11 +27,19 @@ const SoldButton = ({ product_id, isSold, updateIsSold }) => {
   return (
     <button
       onClick={handleClick}
+      className={
+        "w-full font-bold " +
+        (
+          isSold ?
+            soldClasses :
+            unsoldClasses
+        )
+      }
     >
       {
         isSold ?
-          <IconedLabel icon={faXmark} text="Desmarcar como vendido"/> :
-          <IconedLabel icon={faCheck} text="Marcar como vendido"/>
+          <IconedLabel icon={faXmark} text="Desmarcar como vendido" /> :
+          <IconedLabel icon={faCheck} text="Marcar como vendido" />
       }
     </button>
   );
