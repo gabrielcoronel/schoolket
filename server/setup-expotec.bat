c52b3ce0b60d9e1a661c266f5fdb76883cf41c65
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2022-09-28 13:32:05 -0600

    Arreglé el script de inicialización para Windows

--- server/setup-expotec.bat
+++ server/setup-expotec.bat
@@ -1,8 +1,6 @@
-# Versión para Windows (falta probarlo)
-
 @echo off
 
-echo "Configuración de usuario | Ingrese la contraseña del usuario root del sistema operativo"
-mysql "-u" "root" REM UNKNOWN: {"type":"Redirect","op":{"text":"<","type":"less"},"file":{"text":"./sql-scripts/user-setup.txt","type":"Word"}}
-echo "Configuración de base de datos | Ingrese la contraseña del usuario de la base de datos (martir)"
-mysql "-u" "expotec" "-p" REM UNKNOWN: {"type":"Redirect","op":{"text":"<","type":"less"},"file":{"text":"./sql-scripts/database-setup.txt","type":"Word"}}
+set PATH=%PATH%;C:\Program Files\MariaDB 10.9\bin\
+
+mysql -u root < ./sql-scripts/user-setup.txt
+mysql -u expotec -p < ./sql-scripts/database-setup.txt
