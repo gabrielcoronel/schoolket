b106248b8b8c2987812a8a36976a482ee8c86f3f
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2022-09-28 19:32:45 -0600

    Actualicé los scripts de inicialización para que crearan las carpetas necesarias

--- server/setup-expotec.bat
+++ server/setup-expotec.bat
@@ -4,3 +4,6 @@ set PATH=%PATH%;C:\Program Files\MariaDB 10.9\bin\
 
 mysql -u root < ./sql-scripts/user-setup.txt
 mysql -u expotec -p < ./sql-scripts/database-setup.txt
+
+mkdir student_avatars
+mkdir product_pictures
--- server/setup-expotec.sh
+++ server/setup-expotec.sh
@@ -1,5 +1,5 @@
-echo "Configuración de usuario | Ingrese la contraseña del usuario root del sistema operativo"
 sudo mysql -u root < ./sql-scripts/user-setup.txt
-
-echo "Configuración de base de datos | Ingrese la contraseña del usuario de la base de datos (martir)"
 mysql -u expotec -p < ./sql-scripts/database-setup.txt
+
+mkdir student_avatars
+mkdir product_pictures
