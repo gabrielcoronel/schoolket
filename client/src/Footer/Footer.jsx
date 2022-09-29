19082e738ea075143a472140af17c0aefad65aea
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2022-09-29 08:39:01 -0600

    Cambié el Footer para que sea decente

--- client/src/Footer/Footer.jsx
+++ client/src/Footer/Footer.jsx
@@ -1,26 +1,25 @@
 import './Footer.css';
-import logo from 'logo.jpeg';
+import logo from './logo.jpeg';
 
 const Footer = () => {
   return (
     <div className="flex flex-row justify-evenly items-center w-full bg-blue-800 text-white mt-8 p-2">
       <div className="flex flex-row justify-center items-center p-4">
-        <span className="text-5xl text-center">
+        <span className="text-3xl text-center">
           Schoolket
         </span>
       </div>
 
-      <div className="flex flex-row justify-center items-center">
-        <img className="rounded-full h-40 w-40"
+      <div className="flex flex-row justify-center items-center p-4">
+        <img className="rounded-full h-24 w-24 border border-blue-800"
           src={logo}
           alt=""
         />
       </div>
 
       <div className="flex flex-col justify-evenly items-start text-md p-4">
-        <span>Antonio Arrieta Nuñez</span>
-        <span>José Gabriel Coronel Cascante</span>
-        <span>Cristian Valerín Ruiz</span>
+        <span>Departamento de TICs&nbsp;&copy;</span>
+        <span>Expotec 2022</span>
       </div>
     </div>
   );
